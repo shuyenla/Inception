2c1ac34fd98244edc947abefadd98cc72d258bef
shlu <shuyenlu@gmail.com>
2021-12-23 03:33:45 +0100

    stop redirect to example.com but no css so can not post

--- srcs/requirements/wordpress/tools/wp_start.sh
+++ srcs/requirements/wordpress/tools/wp_start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 wp core is-installed --path=/var/www/wordpress --allow-root
 if [ $? == 1 ];then
-    wp core install --url= --path=/var/www/wordpress --title="Wordpress for Inception" --admin_user="shlu" --admin_password="password" --admin_email=shlu@student.42.fr --skip-email --allow-root
-    wp user create test test@tester.com --path=/var/www/wordpress --role=editor --allow-root
+    wp core install --url="shlu.42.fr" --path=/var/www/wordpress --title="Wordpress for Inception" --admin_user="shlu" --admin_password="password" --admin_email=shlu@student.42.fr --skip-email --allow-root
+    wp user create cutie cutie@cutiee.com --path=/var/www/wordpress --role=editor --allow-root
 fi
 
 php-fpm7.3 -F
